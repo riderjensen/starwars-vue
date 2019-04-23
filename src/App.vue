f38b66686d1d193cee40a485d99adb3859002d97
rider <Riderjensen@gmail.com>
2019-04-23 17:40:35 -0600

    Deleting included code, adding card

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,50 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
     <v-content>
-      <HelloWorld/>
+      <v-progress-circular v-if="loading" indeterminate color="primary"></v-progress-circular>
+      <v-container v-bind="{ [`grid-list-md`]: true }" fluid>
+        <v-layout v-if="!loading" row wrap>
+          <v-flex v-for="(starship, index) in starships" :key="index" xs3>
+            <ShipCard />
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import ShipCard from "./components/ShipCard";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    ShipCard
   },
   created() {
-
+    this.axiosCall("https://swapi.co/api/starships/");
   },
-  data () {
+  data() {
     return {
-      starships: []
-    }
+      starships: [],
+      loading: true
+    };
   },
   methods: {
-    axiosCall: function(URL){
-      this.axios.get(URL).then((response) => {
-        console.log(response.data)
-      }).catch(err => console.log(err));
+    axiosCall: function(URL) {
+      this.axios
+        .get(URL)
+        .then(response => {
+          console.log(response.data);
+          this.starships.push(...response.data.results);
+          if (response.data.next) {
+            this.axiosCall(response.data.next);
+          } else {
+            this.loading = false;
+          }
+        })
+        .catch(err => console.log(err));
     }
   }
-}
+};
 </script>
