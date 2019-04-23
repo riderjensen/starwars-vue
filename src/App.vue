41d791471b8db21ac5146ee0bcbe3685cae3de41
rider <Riderjensen@gmail.com>
2019-04-23 17:53:42 -0600

    Main card up and running

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
   <v-app>
     <v-content>
-      <v-progress-circular v-if="loading" indeterminate color="primary"></v-progress-circular>
-      <v-container v-bind="{ [`grid-list-md`]: true }" fluid>
-        <v-layout v-if="!loading" row wrap>
-          <v-flex v-for="(starship, index) in starships" :key="index" xs3>
-            <ShipCard />
-          </v-flex>
-        </v-layout>
-      </v-container>
+     <v-layout>
+      <v-flex xs12 sm10 offset-sm1>
+        <v-card>
+          <v-progress-circular v-if="loading" indeterminate color="primary"></v-progress-circular>
+          <v-container v-bind="{ [`grid-list-md`]: true }" fluid>
+            <v-layout v-if="!loading" row wrap>
+              <v-flex v-for="(starship, index) in starships" :key="index" xs3>
+                <v-card min-height="100%">
+                  <v-card-title primary-title>
+                    <h3 class="headline mb-0">{{starship.name}}</h3>
+                  </v-card-title>
+                  <v-card-actions>
+                    <v-btn class="my-button" flat color="blue">View</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
     </v-content>
-  </v-app>
+  </v-app>  
 </template>
 
 <script>
-import ShipCard from "./components/ShipCard";
 
 export default {
   name: "App",
-  components: {
-    ShipCard
-  },
   created() {
     this.axiosCall("https://swapi.co/api/starships/");
   },
@@ -35,11 +44,11 @@ export default {
       this.axios
         .get(URL)
         .then(response => {
-          console.log(response.data);
           this.starships.push(...response.data.results);
           if (response.data.next) {
             this.axiosCall(response.data.next);
           } else {
+            console.log(this.starships)
             this.loading = false;
           }
         })
@@ -48,3 +57,17 @@ export default {
   }
 };
 </script>
+
+<style>
+.v-card__actions {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+.my-button {
+  width: 100%;
+}
+.v-card__title--primary {
+  margin-bottom: 40px;
+}
+</style>
